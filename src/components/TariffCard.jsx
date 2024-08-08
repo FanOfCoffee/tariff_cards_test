282ab9976ba77bf7e319287888d2dc7d316eeada
FanOfCoffee <keikoshatko@gmail.com>
2024-08-08 15:04:19 +0300

    Pricing

--- src/components/TariffCard.jsx
+++ src/components/TariffCard.jsx
@@ -6,7 +6,18 @@ const TariffCard = ({ title, price, speed, traffic }) => {
         <div className="TariffCard">
             <header>
             <p className="card-title">{title}</p>
-            <h1 className="card-price">{price}</h1>
+            <div className='pricing-price'>
+                <span className='rub'>
+                    <p>руб</p>  
+                </span>
+                <span className='price-card'>
+                    <h1 className="card-price">{price}</h1>
+                </span>
+                <span className='month'>
+                    <p>мес</p>
+                </span>
+            </div>
+            
             </header>
             <div className="card-features">
             <div className="card-speed">{speed}</div>
