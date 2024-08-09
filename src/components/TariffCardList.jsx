96296ff68b51dc8a64d1c91953a3b6150864bf8a
FanOfCoffee <keikoshatko@gmail.com>
2024-08-09 17:59:00 +0300

    Corrections in styles

--- src/components/TariffCardList.jsx
+++ src/components/TariffCardList.jsx
@@ -1,33 +1,31 @@
 import React from 'react'
 import TariffCard from './TariffCard'
-import { useState } from "react";
 
 
 function TariffCardList() {
-    const [selectMonthly, setSelectMonthly] = useState(true);
     return (
         <React.Fragment>
             <TariffCard
             title="Безлимитный 300"
-            price={selectMonthly ? "300" : "3000"}
+            price={"300"}
             speed="до 10 Мбит/сек"
             traffic="Объем включенного трафика не ограничен"
             />
             <TariffCard
                 title="Безлимитный 450"
-                price={selectMonthly ? "450" : "4500"}
+                price={"450"}
                 speed="до 50 Мбит/сек"
                 traffic="Объем включенного трафика не ограничен"
             />
             <TariffCard
                 title="Безлимитный 550"
-                price={selectMonthly ? "550" : "5500"}
+                price={"550"}
                 speed="до 100 Мбит/сек"
                 traffic="Объем включенного трафика не ограничен"
             />
             <TariffCard
                 title="Безлимитный 1000"
-                price={selectMonthly ? "1000" : "10000"}
+                price={"1000"}
                 speed="до 200 Мбит/сек"
                 traffic="Объем включенного трафика не ограничен"
             />
